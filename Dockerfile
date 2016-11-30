2e86ddbe93b3aa27c037c25898fdc2c615f04b1c
BtbN <btbn@btbn.de>
2016-11-30 14:21:34 +0100

    Updates

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM debian:jessie
+FROM ubuntu:16.04
 
 ENV DEBIAN_FRONTEND noninteractive
 
 RUN \
 	apt-get -y update && \
-	apt-get -y install build-essential yasm pkg-config
+	apt-get -y install build-essential yasm pkg-config git && \
+	apt-get -y clean && \
+	rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ADD build_script.sh /root/build_script.sh
 
--- build_script.sh
+++ build_script.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
-git clone https://git.videolan.org/git/ffmpeg.git /root/ffmpeg
+
+git clone --depth=1 https://git.videolan.org/git/ffmpeg.git /root/ffmpeg
 cd /root/ffmpeg
-./configure --enable-gpl --enable-nonfree --enable-version3 --enable-debug=3 --assert-level=2
+
+echo "Configuring..."
+./configure --enable-gpl --enable-nonfree --enable-version3 --enable-debug=3 --assert-level=2 --cpu=core2 \
+	--enable-libspeex --enable-libvpx --enable-libopenjpeg --enable-pthreads --enable-libvorbis \
+	--enable-libx264 --enable-libdc1394 --enable-libxvid --enable-libfreetype \
+	--enable-libmodplug --enable-libpulse --enable-libmp3lame --enable-libbluray --enable-x11grab \
+	--enable-libtheora --enable-openssl --enable-libschroedinger --enable-libcaca --enable-libopus \
+	--enable-libcdio --enable-openal --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libvo-amrwbenc --enable-libgsm \
+	--enable-vdpau --enable-vaapi --enable-iconv  --enable-libgme --enable-libwebp  --enable-libpuls \
+	--enable-libass --enable-frei0r --enable-libsoxr --enable-libfdk-aac --enable-avisynth --enable-libilbc \
+	--enable-libxavs --enable-fontconfig --enable-libbs2b --enable-libzvbi --enable-libtwolame --enable-libsmbclient \
+	--enable-cuda --enable-cuvid --enable-libnpp
 cov-build --dir cov-int make -j4 all alltools examples testprogs
 tar czvf project.tgz README cov-int
